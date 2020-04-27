f7e01089f444e5f030e8de16cba41def533c9185
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-04-27 15:26:34 +0200

    add createTextNode to make a text that can be added to our element and appendChild this text to our element

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 
 function greet(timeString){
-  const hour = timeString.split(":")
+  const hour = timeString.split(":") //add split function to remove colon from string, use parseInt with only first argument to turn string to integer
   if (  parseInt(hour[0]) < 12 ) {
     return `Good Morning`
   } else if (parseInt(hour[0]) > 17){
@@ -20,14 +20,29 @@ function greet(timeString){
   } else {
     return`Good Afternoon`
   }
-  
+  //add else if statements with condition in parenthesis and logic/action in curly braces
 
 }
 /* Write your implementation of displayMessage() */
 
-function displayMessage(){
-  console.log(greet)
+function displayMessage(string){
+ let greeting = document.getElementById("greeting")
+ greeting.innerHTML = string
+//  let text = document.createTextNode(string)
+//  greeting.appendChild(text)
 }
+
+// let newHeader = document.createElement("h1");
+
+// newHeader.setAttribute("id", "victory");
+
+// document.body.appendChild(newHeader);
+
+// // let newHeader = document.querySelector("h1#victory");
+
+// let text = document.createTextNode("YOUR-NAME is the champion");
+
+// newHeader.appendChild(text);
 
 // function addingEventListener() {
 //   const input = document.getElementById('input');
