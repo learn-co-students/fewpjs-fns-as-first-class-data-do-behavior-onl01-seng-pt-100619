4233d9b8d5447076cfdb3d652b66cfa8ffc9a04c
kelly_maass <kmaass77627@gmail.com>
2020-05-25 00:05:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-/* Given Code, don't edit */
-
-document.querySelector("button").addEventListener("click", handleClick);
-
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
 
-/* End Given Code, don't edit above here...*/
+function greet(time) {
+	let timeArray = time.split(":")
+	let hour = parseFloat(timeArray[0])
+	if (hour === NaN) {
+		return "Incorrect format"
+	}
+	else if (hour < 12) {
+		return "Good Morning"
+	} 
+	else if (hour > 17) {
+		return "Good Evening"
+	}
+	else if (hour >= 12 && hour <= 17) {
+		return "Good Afternoon"
+	}
+}
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function displayMessage(string) {
+	document.querySelector("#greeting").innerText = string
+	return
+}
